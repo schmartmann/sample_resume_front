6c10597eb990b787a64a65ac3835c35f813e1062
Jared <jaredcmurphy@gmail.com>
2016-06-05 04:59:33 -0400

    updates landing component and package.json

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React Boilerplate!</div>
+      <div>Your Pleasant Ghost evironment is set up correctly.</div>
     );
   }
 }
