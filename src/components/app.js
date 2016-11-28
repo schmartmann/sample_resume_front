28f4d69e4edc1bb1e28ad09a1ed3cb3619f14d85
Stefan Hartmann <stefanhartmann@gmail.com>
2016-11-27 20:12:59 -0500

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>Your Pleasant Ghost evironment is set up correctly.</div>
+      <div>crowded coding challenge</div>
     );
   }
 }
